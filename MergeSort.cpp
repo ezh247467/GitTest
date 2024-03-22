4ce251fda9049f27e4874478cdef8b48832f30d0
ezh247467 <e8huang@ucsd.edu>
2024-03-22 15:21:44 -0700

    Ethan's changes to MergeSort

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -2,8 +2,22 @@
 #include <vector>
 using namespace std;
 
+
+
+vector<int>* MergeSort(vector<int>* arr) {
+	if (arr.size <= 1) {
+		return arr;
+	}
+	vector<int>* left = MergeSort(
+}
+
 int main() {
 	cout << "Hello This Is Joshua" << endl;
 	cout << "This is Joshua I'm Conflicting" << endl;
+	vector<int> test = {1, 2, 3};
+	vector<int> tester(test.begin(), test.end() - (test.size() / 2));
+	for (auto i : tester) {
+		cout << i << endl;
+	}
 	return 0;
 }
